98a8650fd3c4ab193f8c42cd503b682b2cba8e82
Jordan <jordanrowland00@gmail.com>
2019-04-13 19:15:00 +0000

    adds smooth scrolling for header, aligns scroll anchors

--- app/main/views.py
+++ app/main/views.py
@@ -11,20 +11,6 @@ from ..models import Post
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    # form = ContactForm()
-    # if form.validate_on_submit():
-    #     name = form.name.data
-    #     email = form.email.data
-    #     subject = form.subject.data
-    #     msg = form.message.data
-        # send_email(
-        #     to='JordanRowland00@gmail.com',
-        #     subject=f'New message from {name} - {email}',
-        #     msg_body=msg,
-        #     template='email')
-        # flash('Message Sent!')
-        # return redirect(url_for('.index', _anchor="contact"))
-
 
     data = request.get_json()
     print(data)
@@ -45,22 +31,6 @@ def index():
     return render_template(
         'index.html',
         )
-
-
-@main.route('/email')
-def email():
-    print(
-    request.get_json()
-        )
-    send_email(
-        to='JordanRowland00@gmail.com',
-        subject=f'New message from {name} - {email}',
-        msg_body=msg,
-        template='email'
-        )
-
-
-
 
 
 @main.route('/blog')
