10ab7dc35027caf2bd90317936f54ded8c8369c1
Jordan Rowland <JordanRowland00@gmail.com>
2019-04-28 13:18:44 -0700

    gets app ready for deploy

--- blog.py
+++ blog.py
@@ -3,8 +3,19 @@ import sys
 import click
 
 from app import create_dev_app, create_prod_app, db
+from flask_migrate import upgrade
+from flask_script import Manager
 from app.models import Post, User
 from flask_migrate import Migrate
+
+
+manager = Manager()
+
+
+@manager.command
+def deploy():
+	upgrade()
+
 
 if os.getenv('FLASK_CONFIG') == 'heroku' \
 or os.getenv('FLASK_CONFIG') == 'production':
